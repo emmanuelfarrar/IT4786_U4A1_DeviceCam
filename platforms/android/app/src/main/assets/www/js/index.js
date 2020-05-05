3df22159eaa0dad42cc48b27f86e5a6fc336e2c9
Emmanuel Farrar <6923873+emmanuelfarrar@users.noreply.github.com>
2020-05-05 14:27:10 +0300

    Initial commit. Need to style the project.

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,43 @@
+var cameraOpts;     
+
+window.onload = function() {
+    this.document.addEventListener('deviceready', init, false);
+}
+
+/** init()
+ * sets the options that we pass to the phone's camera.
+ */
+function init() {
+    cameraOpts = {
+        quality: 100,
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG,
+      };
+}
+
+/** takePic()
+ * calls the getPicture() to launch native camera app w/ the set camera options.
+ * If the picture is successful then camSuccess() is called if not the camFail()
+ */
+function takePic() {
+    navigator.camera.getPicture(camSuccess, camFail, camerOpts);
+}
+
+/**cameraSuccess(imageData)
+ * sets the picture as the source of resultsImg <img> element
+ * @param {*} imageData 
+ */
+function camSuccess(imageData){
+    var lastPhoto = document.getElementById('resultsImg');
+    lastPhoto.src = imageData;
+}
+
+/**cameraFail(message)
+ * displays the failure message in an alert window
+ * @param {*} message 
+ */
+function cameFail(message){
+    alert("Error: " + message);
+  }
